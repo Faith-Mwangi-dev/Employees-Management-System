e98046bee8095a71ec2a79fbb6a76d2193bd3afa
Faith-Mwangi-dev <faymwangi27@gmail.com>
2026-06-13 22:28:02 +0300

    Updated rights on the system

--- actions/authenticate.php
+++ actions/authenticate.php
@@ -6,11 +6,9 @@ include "../includes/db.php";
 
 $username = $_POST['username'] ?? '';
 $password = $_POST['password'] ?? '';
-$user_id = $_POST['user_id'] ?? '';
 
-$sql = "SELECT * FROM admins 
-        WHERE username='$username' 
-        AND password='$password'";
+$sql = "SELECT * FROM admins
+        WHERE username='$username'";
 
 $result = $conn->query($sql);
 
@@ -18,16 +16,24 @@ if ($result && $result->num_rows > 0) {
 
     $row = $result->fetch_assoc();
 
-    $_SESSION['user_id'] = $row['id'];
-    $_SESSION['username'] = $row['username'];
-    $_SESSION['role'] = strtolower(trim($row['role']));
-    header("Location: ../index.php");
-    exit();
+    if (password_verify($password, $row['password'])) {
+
+        $_SESSION['user_id'] = $row['id'];
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['role'] = strtolower(trim($row['role']));
+
+        header("Location: ../index.php");
+        exit();
+
+    } else {
+
+        echo "Invalid username or password";
+
+    }
 
 } else {
 
     echo "Invalid username or password";
 
 }
-
 ?>
